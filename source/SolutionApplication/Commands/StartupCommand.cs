a8a9e172941531c6c2bc1dd0062042a23af4a661
Georg Orlov <orlov@altek.online>
2025-01-10 17:13:37 +0500

    I'm writing a handler to calculate the height of the wall.

--- source/SolutionApplication/Commands/StartupCommand.cs
+++ source/SolutionApplication/Commands/StartupCommand.cs
@@ -1,4 +1,5 @@
 ﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.UI.Selection;
 using Nice3point.Revit.Toolkit.External;
@@ -24,7 +25,74 @@ namespace SolutionApplication.Commands
 
             TaskDialog.Show("Информация", $"Id элемента: {id.ToString()}" + $"\n\nТип элемента: {categoryElement}");
 
+            //Parameter widthParam = element.LookupParameter("Width");
+
+            //TaskDialog.Show("Информация", $"Width элемента = {widthParam.ToString()}");
+
+            //GetWidth(id);
+            GetHeightWall(element);
+
             return Result.Succeeded;
+        }
+
+        // Высота стены
+        public double GetHeightWall(Element element)
+        {
+            //TaskDialog.Show("Информация", $"Id элемента: {idElement.ToString()}");
+
+            ElementId id = element.Id;
+            string categoryElement = element.Category.Name.ToString();
+
+            if (categoryElement == "Walls")
+            {
+                TaskDialog.Show("Информация", "Элемент является стеной, вычисление высоты будет корректным");
+            }
+            else
+            {
+                TaskDialog.Show("Информация", "Элемент НЕ является стеной!\n\ncategoryElement = " + categoryElement);
+            }
+
+            return 0f;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // Переводит числовое значение из метров в футы
+        public double ConvertMetersToFoot(double inputVal)
+        {
+            inputVal = inputVal / 3.048f;
+            inputVal = Math.Round(inputVal, 2);
+
+            return (inputVal);
+        }
+
+        // Переводит числовое значение из футов в метры
+        public double ConvertFootToMeters(double inputVal)
+        {
+            inputVal = inputVal * 3.048f;
+            inputVal = Math.Round(inputVal, 2);
+
+            return (inputVal);
         }
     }
 }
