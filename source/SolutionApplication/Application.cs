9c20b092261083c71a4cf1c2fbbfdc69af63464d
Georg Orlov <orlov@altek.online>
2025-01-10 15:16:03 +0500

    First commit

---
+++ source/SolutionApplication/Application.cs
@@ -0,0 +1,23 @@
+﻿using Nice3point.Revit.Toolkit.External;
+using SolutionApplication.Commands;
+
+namespace SolutionApplication
+{
+    [UsedImplicitly]
+    public class Application : ExternalApplication
+    {
+        public override void OnStartup()
+        {
+            CreateRibbon();
+        }
+
+        private void CreateRibbon()
+        {
+            var panel = Application.CreatePanel("Commands", "RevitAddIn16");
+
+            panel.AddPushButton<StartupCommand>("Кнопка 1")
+                .SetImage("/SolutionApplication;component/Resources/Icons/RibbonIcon16.png")
+                .SetLargeImage("/SolutionApplication;component/Resources/Icons/RibbonIcon32.png");
+        }
+    }
+}
---
+++ source/SolutionApplication/Commands/StartupCommand.cs
@@ -0,0 +1,19 @@
+﻿using Autodesk.Revit.Attributes;
+using Module_1.ViewModels;
+using Module_1.Views;
+using Nice3point.Revit.Toolkit.External;
+
+namespace SolutionApplication.Commands
+{
+    [UsedImplicitly]
+    [Transaction(TransactionMode.Manual)]
+    public class StartupCommand : ExternalCommand
+    {
+        public override void Execute()
+        {
+            var viewModel = new Module_1ViewModel();
+            var view = new Module_1View(viewModel);
+            view.ShowDialog();
+        }
+    }
+}
