5ae470835206a83196540df0bcaf8d8f4df79269
Georg Orlov <orlov@altek.online>
2025-01-13 16:16:33 +0500

    Configured correct calc and display langth, height and width wall and window

--- source/SolutionApplication/Commands/StartupCommand.cs
+++ source/SolutionApplication/Commands/StartupCommand.cs
@@ -1,4 +1,5 @@
-﻿using Autodesk.Revit.Attributes;
+﻿using System.Xaml;
+using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.UI.Selection;
@@ -61,29 +62,96 @@ namespace SolutionApplication.Commands
 
                 // wall width - ширина (глубина)
 
-                //width = element.LookupParameter("Width").AsDouble();
-                // Definition: HOST_VOLUME_COMPUTED / 
-                // Definition: WALL_ATTR_WIDTH_PARAM
-
-                //width = element.get_Parameter(BuiltInParameter.WALL_ATTR_WIDTH_PARAM).AsDouble();
-
                 Wall wall = (Wall)element;
-                WallType wallType = wall.WallType; // Получаю тип стены
+                WallType wallType = wall.WallType; // Получаю тип текущей стены
 
                 // И у типа стены уже есть параметр width
                 width = wallType.get_Parameter(BuiltInParameter.WALL_ATTR_WIDTH_PARAM).AsDouble();
 
                 width = ConvertFootToMeters(width);
 
+                // Преобразуем в см
+                length = length * 10;
+                height = height * 10;
+                width = width * 10;
 
                 TaskDialog.Show("Информация",
-                    "wall length = " + length + " метров" +
-                    "\nwall height = " + height + " метров" +
-                    "\nwall width = " + width + " метров");
+                    "wall length = " + length + " см" +
+                    "\nwall height = " + height + " см" +
+                    "\nwall width = " + width + " см");
+            }
+            else if (categoryElement == "Windows")
+            {
+                double buferHeight = 0;
+                double buferWidth = 0;
+
+                // INSTANCE_HEAD_HEIGHT_PARAM
+                // width = walltype
+                // length = HOST_AREA_COMPUTED / height
+
+                // wall length - длинна 
+
+                length = element.get_Parameter(BuiltInParameter.HOST_AREA_COMPUTED).AsDouble();
+                //
+
+                // wall height - высота
+
+                height = element.get_Parameter(BuiltInParameter.INSTANCE_HEAD_HEIGHT_PARAM).AsDouble();
+                buferHeight = height;
+                height = ConvertFootToMeters(height);
+
+
+
+                // wall width - ширина (глубина)
+
+                // Проверяем, является ли элемент экземпляром семейства и категорией окон
+                if (element is FamilyInstance familyInstance && familyInstance.Category.Id.IntegerValue == (int)BuiltInCategory.OST_Windows)
+                {
+                    // Получаем элемент-хозяина (host) для окна
+                    Element hostElement = familyInstance.Host;
+
+                    if (hostElement is Wall)
+                    {
+                        Wall hostWall = (Wall)hostElement;
+                        //TaskDialog.Show("Стенка-хозяин", "Id стенки-хозяина: " + hostWall.Id);
+
+                        WallType wallType = hostWall.WallType;
+                        width = wallType.get_Parameter(BuiltInParameter.WALL_ATTR_WIDTH_PARAM).AsDouble();
+                        buferWidth = width;
+                        width = ConvertFootToMeters(width);
+                    }
+                    else
+                    {
+                        TaskDialog.Show("Ошибка", "Окно не прикреплено к стене.");
+                    }
+                }
+                else
+                {
+                    TaskDialog.Show("Ошибка", "Выбранный элемент не является окном.");
+                }
+
+
+                length = length / buferHeight / buferWidth;
+                length = ConvertFootToMeters(length);
+
+                // Преобразуем в см
+                length = length * 10;
+                height = height * 10;
+                width = width * 10;
+
+
+                TaskDialog.Show("Информация",
+                    "wall length = " + length + " см" +
+                    "\nwall height = " + height + " см" +
+                    "\nwall width = " + width + " см");
+            }
+            else if (categoryElement == "Doors")
+            {
+
             }
             else
             {
-                TaskDialog.Show("Информация", "Элемент НЕ является стеной!\n\ncategoryElement = " + categoryElement);
+                TaskDialog.Show("Информация", "Неизвестный тип элемента!\n\ncategoryElement = " + categoryElement);
             }
 
             return height;
