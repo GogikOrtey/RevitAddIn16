5a10fa6ba915336dd2cae36a10ef75bc6734760a
Georg Orlov <orlov@altek.online>
2025-01-14 13:30:02 +0500

    Test work

--- source/SolutionApplication/Commands/StartupCommand.cs
+++ source/SolutionApplication/Commands/StartupCommand.cs
@@ -28,15 +28,15 @@ namespace SolutionApplication.Commands
             ElementId id = element.Id;
             string categoryElement = element.Category.Name.ToString();
 
-            TaskDialog.Show("Информация", $"Id элемента: {id.ToString()}" + $"\n\nТип элемента: {categoryElement}");
-
+            //TaskDialog.Show("Информация", $"Id элемента: {id.ToString()}" + $"\n\nТип элемента: {categoryElement}");
             //Parameter widthParam = element.LookupParameter("Width");
-
             //TaskDialog.Show("Информация", $"Width элемента = {widthParam.ToString()}");
 
-            //GetWidth(id);
             GetHeightWall(element);
-            //GetDimensionsObject(element);
+
+            // Получение длины объекта element
+            double length = ElementProcessor.GetLength(element);
+            TaskDialog.Show("Информация", "length(element) = " + length);
 
             return Result.Succeeded;
         }
@@ -51,7 +51,7 @@ namespace SolutionApplication.Commands
             double Blength = 0;
 
             if (boundingBox != null)
-            {                
+            {
                 XYZ minPoint = boundingBox.Min;
                 XYZ maxPoint = boundingBox.Max;
 
@@ -77,16 +77,38 @@ namespace SolutionApplication.Commands
             return 0;
         }
 
-        // Высота стены
+        //public void ProcObjForHisType(Element element)
+        //{
+        //    ElementId id = element.Id;
+        //    string categoryElement = element.Category.Name.ToString();
+
+        //    if (categoryElement == "Walls")
+        //    {
+        //        Wall wall = (Wall)element;
+        //    }
+        //    else if (categoryElement == "Windows")
+        //    {
+        //        Window window = (Window)element;
+        //    }
+        //    else if (categoryElement == "Doors")
+        //    {
+                
+        //    }
+        //    else
+        //    {
+        //        TaskDialog.Show("Информация", "Неизвестный тип элемента!\n\ncategoryElement = " + categoryElement);
+        //    }
+        //}
+
+        // Получение длины ширины и высоты объекта
+        // Обработка разными методами, в зависимости от типа объекта
         public double GetHeightWall(Element element)
         {
-            //TaskDialog.Show("Информация", $"Id элемента: {idElement.ToString()}");
-
             ElementId id = element.Id;
             string categoryElement = element.Category.Name.ToString();
 
             double length = 0;
-            double height = 0;            
+            double height = 0;
             double width = 0;
 
             if (categoryElement == "Walls")
@@ -113,7 +135,7 @@ namespace SolutionApplication.Commands
                 // Преобразуем в см
                 length = length * 10;
                 height = height * 10;
-                width  = width  * 10;
+                width = width * 10;
 
                 TaskDialog.Show("Информация",
                     "wall length = " + length + " см" +
@@ -124,54 +146,16 @@ namespace SolutionApplication.Commands
             {
                 // Буферные значения размеров в футах, для корректного рассчёта длинны
                 double buferHeight = 0;
-                double buferWidth = 0;
-
-                // INSTANCE_HEAD_HEIGHT_PARAM
-                // width = walltype
-                // length = HOST_AREA_COMPUTED / height
 
                 // length - длинна 
 
                 length = element.get_Parameter(BuiltInParameter.HOST_AREA_COMPUTED).AsDouble();
-                //
 
                 // height - высота
 
                 height = element.get_Parameter(BuiltInParameter.INSTANCE_HEAD_HEIGHT_PARAM).AsDouble();
                 buferHeight = height;
                 height = ConvertFootToMeters(height);
-
-
-
-                //// width - ширина (глубина)
-
-                //// Проверяем, является ли элемент экземпляром семейства и категорией окон
-                //if (element is FamilyInstance familyInstance && familyInstance.Category.Id.IntegerValue == (int)BuiltInCategory.OST_Windows)
-                //{
-                //    // Получаем элемент-хозяина (host) для окна
-                //    Element hostElement = familyInstance.Host;
-
-                //    if (hostElement is Wall)
-                //    {
-                //        Wall hostWall = (Wall)hostElement;
-                //        //TaskDialog.Show("Стенка-хозяин", "Id стенки-хозяина: " + hostWall.Id);
-
-                //        WallType wallType = hostWall.WallType;
-                //        width = wallType.get_Parameter(BuiltInParameter.WALL_ATTR_WIDTH_PARAM).AsDouble();
-                //        buferWidth = width;
-                //        width = ConvertFootToMeters(width);
-                //    }
-                //    else
-                //    {
-                //        TaskDialog.Show("Ошибка", "Окно не прикреплено к стене.");
-                //    }
-                //}
-                //else
-                //{
-                //    TaskDialog.Show("Ошибка", "Выбранный элемент не является окном.");
-                //}
-
-
 
                 // Получаем ограничивающую рамку объекта
                 BoundingBoxXYZ boundingBox = element.get_BoundingBox(null);
@@ -197,14 +181,10 @@ namespace SolutionApplication.Commands
                 width = width / buferHeight / buferLength;
                 width = ConvertFootToMeters(width);
 
-                //length = length / buferHeight / buferWidth;
-                //length = ConvertFootToMeters(length);
-
                 // Преобразуем в см
                 Blength = Blength * 10;
                 height = height * 10;
-                width  = width  * 10;
-
+                width = width * 10;
 
                 TaskDialog.Show("Информация",
                     "windows length = " + Blength + " см" +
@@ -217,82 +197,17 @@ namespace SolutionApplication.Commands
                 double buferLength = 0;
                 double buferHeight = 0;
 
-                // Host -> Length: Definition: CURVE_ELEM_LENGTH 
-                // INSTANCE_HEAD_HEIGHT_PARAM
-                // ??
-
-                // length - длинна 
-
-                //length = element.get_Parameter(BuiltInParameter.CURVE_ELEM_LENGTH).AsDouble();
-                //length = ConvertFootToMeters(length);
-
-                //length = element.get_Parameter(BuiltInParameter.DOOR_HEIGHT).AsDouble();
-                //length = ConvertFootToMeters(length);
-
-                //// Проверяем, является ли элемент экземпляром семейства и категорией дверей
-                //if (element is FamilyInstance familyInstance && familyInstance.Category.Id.IntegerValue == (int)BuiltInCategory.OST_Doors)
-                //{
-                //    // Получаем элемент-хозяина (host) для двери
-                //    Element hostElement = familyInstance.Host;
-
-                //    if (hostElement is Wall)
-                //    {
-                //        Wall hostWall = (Wall)hostElement;
-                //        length = hostWall.get_Parameter(BuiltInParameter.CURVE_ELEM_LENGTH).AsDouble();
-                //        buferLength = length;
-                //        length = ConvertFootToMeters(length);
-                //    }
-                //    else
-                //    {
-                //        TaskDialog.Show("Ошибка", "Дверь не прикреплена к стене.");
-                //    }
-                //}
-                //else
-                //{
-                //    TaskDialog.Show("Ошибка", "Выбранный элемент не является дверью.");
-                //}
-
                 // height - высота
 
                 height = element.get_Parameter(BuiltInParameter.INSTANCE_HEAD_HEIGHT_PARAM).AsDouble();
                 buferHeight = height;
                 height = ConvertFootToMeters(height);
 
-                // ! Корректно работает
-
-
-
-                //width = element.get_Parameter(BuiltInParameter.DOOR_WIDTH).AsDouble();
-                //width = ConvertFootToMeters(width);
-
-                //if (element is FamilyInstance familyInstance && familyInstance.Category.Id.IntegerValue == (int)BuiltInCategory.OST_Doors)
-                //{
-                //    // Получаем параметры ширины и высоты
-                //    double widthParam = familyInstance.get_Parameter(BuiltInParameter.DOOR_WIDTH).AsDouble();
-                //    double heightParam = familyInstance.get_Parameter(BuiltInParameter.DOOR_HEIGHT).AsDouble();
-
-                //    width = widthParam + 1;
-                //    length = heightParam;
-
-                //    width = ConvertFootToMeters(width);
-                //    length = ConvertFootToMeters(length);
-                //}
-
-
-                /*
-                
-                    Не понятно, как получить параметры длины и ширины у двери.
-
-                    Или хотя бы одной величины, а другую я смогу вычислить через объём 
-
-                */
-
+                // length - длинна 
 
                 // Получаем ограничивающую рамку объекта
                 BoundingBoxXYZ boundingBox = element.get_BoundingBox(null);
 
-                //double Bwidth = 0;
-                //double Bheight = 0;
                 double Blength = 0;
 
                 if (boundingBox != null)
@@ -300,16 +215,11 @@ namespace SolutionApplication.Commands
                     XYZ minPoint = boundingBox.Min;
                     XYZ maxPoint = boundingBox.Max;
 
-                    // И вычисляем значени по длине, ширине и высоте
-                    //Bwidth = maxPoint.X - minPoint.X;
                     Blength = maxPoint.Y - minPoint.Y;
-                    //Bheight = maxPoint.Z - minPoint.Z;
                 }
 
                 buferLength = Blength;
 
-                //Bwidth = ConvertFootToMeters(Bwidth);
-                //Bheight = ConvertFootToMeters(Bheight);
                 Blength = ConvertFootToMeters(Blength);
 
                 // width - ширина (глубина)
@@ -323,8 +233,6 @@ namespace SolutionApplication.Commands
                 height = height * 10;
                 width = width * 10;
 
-                //Bwidth = Bwidth * 10;
-                //Bheight = Bheight * 10;
                 Blength = Blength * 10;
 
                 TaskDialog.Show("Информация",
@@ -344,7 +252,7 @@ namespace SolutionApplication.Commands
 
 
 
-      
+
 
 
 
--- source/SolutionApplication/GetDimensionsElement.cs
+++ source/SolutionApplication/GetDimensionsElement.cs
@@ -6,102 +6,156 @@ using System.Threading.Tasks;
 
 namespace SolutionApplication
 {
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     //internal class GetDimensionsElement
     //{ }
 
 
 
-    //// Абстрактный класс
-    //public abstract class BuildingElement
-    //{
-    //    public abstract double GetWidth();
-    //    public abstract double GetLength();
-    //    public abstract double GetWeight();
-    //}
+    // Абстрактный класс
+    public abstract class BuildingElement
+    {
+        public abstract double GetWidth(Element element);
+        public abstract double GetLength(Element element);
+        public abstract double GetWeight(Element element);
+    }
 
-    //// Класс для стены
-    //public class Wall : BuildingElement
-    //{
-    //    // Получение ширины стены
-    //    public override double GetWidth()
-    //    {
-    //        return 0;
-    //    }
+    // Класс для стены
+    public class WallElement : BuildingElement
+    {
+        // Получение ширины стены
+        public override double GetWidth(Element element)
+        {
+            return 1;
+        }
 
-    //    // Получение высоты стены
-    //    public override double GetLength()
-    //    {
-    //        return 0;
-    //    }
+        // Получение высоты стены
+        public override double GetLength(Element element)
+        {
+            return 2;
+        }
 
-    //    // Получение длины стены
-    //    public override double GetWeight()
-    //    {
-    //        return 0;
-    //    }
-    //}
+        // Получение длины стены
+        public override double GetWeight(Element element)
+        {
+            return 3;
+        }
+    }
 
-    //// Класс для окна
-    //public class Window : BuildingElement
-    //{
-    //    // Получение ширины окна
-    //    public override double GetWidth()
-    //    {
-    //        return 0;
-    //    }
+    // Класс для окна
+    public class WindowElement : BuildingElement
+    {
+        // Получение ширины окна
+        public override double GetWidth(Element element)
+        {
+            return 4;
+        }
 
-    //    // Получение высоты окна
-    //    public override double GetLength()
-    //    {
-    //        return 0;
-    //    }
+        // Получение высоты окна
+        public override double GetLength(Element element)
+        {
+            return 5;
+        }
 
-    //    // Получение глубины окна
-    //    public override double GetWeight()
-    //    {
-    //        return 0;
-    //    }
-    //}
+        // Получение глубины окна
+        public override double GetWeight(Element element)
+        {
+            return 6;
+        }
+    }
 
-    //// Класс для двери
-    //public class Door : BuildingElement
-    //{
-    //    // Получение ширины двери
-    //    public override double GetWidth()
-    //    {
-    //        return 0;
-    //    }
+    // Класс для двери
+    public class DoorElement : BuildingElement
+    {
+        // Получение ширины двери
+        public override double GetWidth(Element element)
+        {
+            return 7;
+        }
 
-    //    // Получение высоты двери
-    //    public override double GetLength()
-    //    {
-    //        return 0;
-    //    }
+        // Получение высоты двери
+        public override double GetLength(Element element)
+        {
+            return 8;
+        }
 
-    //    // Получение глубины двери
-    //    public override double GetWeight()
-    //    {
-    //        return 0;
-    //    }
-    //}
+        // Получение глубины двери
+        public override double GetWeight(Element element)
+        {
+            return 9;
+        }
+    }
+
+
+    // Основной метод, который определяет тип элемента и вызывает соответствующий метод
+    public static class ElementProcessor
+    {
+        public static double GetWidth(Element element)
+        {
+            string categoryElement = element.Category.Name.ToString();
+            BuildingElement buildingElement;
+
+            switch (categoryElement)
+            {
+                case "Walls":
+                    buildingElement = new WallElement();
+                    break;
+                case "Windows":
+                    buildingElement = new WindowElement();
+                    break;
+                case "Doors":
+                    buildingElement = new DoorElement();
+                    break;
+                default:
+                    throw new ArgumentException("Неизвестный тип элемента");
+            }
+
+            return buildingElement.GetWidth(element);
+        }
+
+        public static double GetLength(Element element)
+        {
+            string categoryElement = element.Category.Name.ToString();
+            BuildingElement buildingElement;
+
+            switch (categoryElement)
+            {
+                case "Walls":
+                    buildingElement = new WallElement();
+                    break;
+                case "Windows":
+                    buildingElement = new WindowElement();
+                    break;
+                case "Doors":
+                    buildingElement = new DoorElement();
+                    break;
+                default:
+                    throw new ArgumentException("Неизвестный тип элемента");
+            }
+
+            return buildingElement.GetLength(element);
+        }
+
+        public static double GetWeight(Element element)
+        {
+            string categoryElement = element.Category.Name.ToString();
+            BuildingElement buildingElement;
+
+            switch (categoryElement)
+            {
+                case "Walls":
+                    buildingElement = new WallElement();
+                    break;
+                case "Windows":
+                    buildingElement = new WindowElement();
+                    break;
+                case "Doors":
+                    buildingElement = new DoorElement();
+                    break;
+                default:
+                    throw new ArgumentException("Неизвестный тип элемента");
+            }
+
+            return buildingElement.GetWeight(element);
+        }
+    }
 }
