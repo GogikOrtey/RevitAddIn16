0182054e47de17601fa3a69d28f27748d5c63116
Georg Orlov <orlov@altek.online>
2025-01-17 11:49:35 +0500

    Adding comments correctly work

--- source/SolutionApplication/Commands/StartupCommand2.cs
+++ source/SolutionApplication/Commands/StartupCommand2.cs
@@ -1,4 +1,6 @@
-﻿using Autodesk.Revit.Attributes;
+﻿using System.Xml.Linq;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.UI.Selection;
 
@@ -20,31 +22,42 @@ namespace SolutionApplication.Commands
 
             string idElement = element.Id.ToString();
 
-            TaskDialog.Show("Информация", "id объекта = " + idElement);
+            TaskDialog.Show("Информация", "id объекта = " + idElement);          
+
+            // Создаем транзакцию, для добавления комментария к объекту
+            using (Transaction transaction = new Transaction(doc, "Add Comment"))
+            {
+                transaction.Start();
+                try
+                {
+                    element.AddComment("Комментарий 1");
+
+                    transaction.Commit();
+                }
+                catch
+                {
+                    TaskDialog.Show("Информация", "При добавлении комментария к объекту с id = " + idElement + ", произошла ошибка!\n\nКомментарий не добавлен");
+                    transaction.RollBack();
+                    throw;
+                }
+            }
 
             return Result.Succeeded;
         }
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    // Создаём метод расширения для встроенного класса Element
+    public static class ElementExtensions
+    {
+        public static void AddComment(this Element element, string comment)
+        {
+            // Параметр "Комментарии" имеет встроенное имя "ALL_MODEL_INSTANCE_COMMENTS".
+            Parameter commentParameter = element.get_Parameter(BuiltInParameter.ALL_MODEL_INSTANCE_COMMENTS);
+            if (commentParameter != null && !commentParameter.IsReadOnly)
+            {
+                // Установим значение параметра "Комментарии".
+                commentParameter.Set(comment);
+            }
+        }
     }
 }
